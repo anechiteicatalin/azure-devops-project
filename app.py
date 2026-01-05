23a70e29e14f5046c01d5c2b79b4d8987b05b24d
anechiteicatalin <72701272+anechiteicatalin@users.noreply.github.com>
2026-01-05 23:21:58 +0000

    Add Hello World app and Dockerfile

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Azure Kubernetes Service!"
+    return "Hello from Azure Poland Central! 🚀"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
